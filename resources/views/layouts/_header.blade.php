0602bb7032998c118b0d4ac29797cbc184ad8d21
xueshang <1206324968@qq.com>
2020-03-12 02:57:50 +0000

    生成用户认证代码

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -9,16 +9,19 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
       <!-- Left Side Of Navbar-->
       <ul class="navbar-nav mr-auto"></ul>
+
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
+
         <!-- Authentication Links -->
         <li class="nav-item">
-          <a class="nav-link" href="#">登录</a>
+          <a class="nav-link" href="{{ route('login') }}">登录</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">注册</a>
+          <a class="nav-link" href="{{ route('register') }}">注册</a>
         </li>
       </ul>
     </div>
