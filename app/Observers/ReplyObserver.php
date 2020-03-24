ecdf74f228042706ffd900e872bf97b8212d6395
xueshang <1206324968@qq.com>
2020-03-24 08:36:25 +0000

    删除评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -14,8 +14,9 @@ class ReplyObserver
     public function  created(Reply $reply)
     {
         //$reply->topic->increment('reply_count',1);
-        $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
+//        $reply->topic->reply_count = $reply->topic->replies->count();
+//        $reply->topic->save();
+        $reply->topic->updateReplyCount();
 
         //通知话题作者有新的评论
         $reply->topic->user->notify(new TopicReplied($reply));
@@ -24,6 +25,11 @@ class ReplyObserver
     public function creating(Reply $reply)
     {
         $reply->content = clean($reply->content,'user_topic_body');
+    }
+
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->updateReplyCount();
     }
 
 }
