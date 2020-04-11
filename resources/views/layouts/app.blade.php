a0fa488e7a4a41d7f0ad1aefce76c9d91da17492
xueshang <1206324968@qq.com>
2020-04-11 07:53:10 +0000

    站点设置

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,10 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>
-    @yield('title','LaraBBS')-Laravel 进阶教程-XueShangFlight
+    @yield('title','LaraBBS')-{{ setting('site_name','Laravel 进阶教程-XueShangFlight') }}
   </title>
-  <meta name="description" content="@yield('description','LaraBBS xueshang论坛|xueshang社区')">
+  <meta name="description" content="@yield('description',setting('seo_description','LaraBBS xueshang论坛|xueshang社区'))">
+  <meta name="keyword" content="@yield('keyword',setting('seo_keyword','LaraBBS,社区，论坛，开发者论坛'))">
   <!-- Styles-->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
